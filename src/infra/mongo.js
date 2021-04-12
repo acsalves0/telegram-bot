852f27cdba0018ab4d2627351613f61d068fb786
acsalves0 <acsalourencioalves@gmail.com>
2021-04-12 15:10:02 -0300

    v1

---
+++ src/infra/mongo.js
@@ -0,0 +1,32 @@
+const mongoUri = require('../environment').mongoUri
+const {MongoClient} = require('mongodb')
+
+async function conectarMongoDB(){
+    const client = new MongoClient(mongoUri,  { useUnifiedTopology: true });
+
+    try {
+    await client.connect();
+    console.log("Conexão estabelecida com sucesso!!");
+    return client
+    }catch(e){
+        console.log(e);
+    }
+}
+
+async function desconectaMongoDB(client) {
+    if (client) {
+      try {
+        await client.close();
+        console.log("Conexão fechada com sucesso!");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+}
+
+// (async () => {
+//     const cliente = await conectarMongoDB();
+//     await desconectaMongoDB(cliente);
+//   })();
+  
+  module.exports = {conectarMongoDB, desconectaMongoDB}
